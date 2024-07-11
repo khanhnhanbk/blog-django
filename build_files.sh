cf47e14e23728103a9d029fb3cad779ef2631d60
khanhnhanbk <nhanhoa21012002@gmail.com>
2024-07-11 14:55:02 +0700

    chore: Update build_files.sh to include virtual environment activation

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,10 @@
 # build_files.sh
+# create a virtual environment named 'venv' if it doesn't already exist
 python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
 
 pip install -r requirements.txt
 
